af44b735859e15b89a2f1ef8d91ad278c232e5cc
Manikandan <98687986+MaNiKaNdAn0303@users.noreply.github.com>
2022-06-25 22:49:10 +0530

    Add files via upload

---
+++ src/Components/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Application from '../Components/Application';
+
+function Main() {
+  return (
+    <>
+      <Application />
+
+
+    </>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+export default Main;
